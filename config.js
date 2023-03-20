b564057c19e4ec919385bedce68eea90ad704791
Madhukar <madhukar@sg-lt-233.senecaglobal.net>
2023-03-20 18:15:36 +0530

    delete symptom

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 
-
 const config = {
   db: {
-      host: "localhost",
-      user: "root",
-      password: "madhu",
-      database: "projectdb",
-    }
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12606962',
+    password: 'ueiLzEp7Bt',
+    database: 'sql12606962',
+    port: 3306
   }
-  
+};
   module.exports = config;
 
 
